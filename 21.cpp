fb60b03529b0bbff77a02ee2ee00c126fa467a74
xup6tp6u4 <48340287+xup6tp6u4@users.noreply.github.com>
2019-05-28 01:34:53 +0800

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Servitor{
+	char id[20];
+	char name[20];
+	int wage;
+	int hour;
+	int pay;
+};
+int salc(struct Servitor*);
+int main(void){
+	struct Servitor servitor;
+	printf("請輸入 id:");
+	scanf("%s", servitor.id);
+	printf("請輸入 name:");
+	scanf("%s", servitor.name);
+	printf("請輸入 wage:");
+	scanf("%d", &servitor.wage);
+	printf("請輸入 hour:");
+	scanf("%d", &servitor.hour);
+	salc(&servitor);
+	printf("\n\n%s的資料:---------\n", servitor.name);
+	printf("ID:%s\n", servitor.id);
+	printf("姓名:%s\n", servitor.name);
+	printf("時薪:%d\n", servitor.wage);
+	printf("工作時數:%d:\n", servitor.hour);
+	printf("薪水:%d\n", servitor.pay);
+}
+int salc(struct Servitor *ser){
+	(ser->pay)=(ser->wage)*(ser->hour);
+	return (ser->pay);
+}
